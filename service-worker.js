a4754a4aa405a611c7e453136a01e2aaaa039105
GreenIce99 <6508roshan@gmail.com>
2025-07-15 12:19:45 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,26 @@
-const CACHE_NAME = 'dino-clicker-cache-v1';
-const urlsToCache = [
+const cacheName = 'dino-clicker-cache-v1';
+const assetsToCache = [
   './',
   './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.json'
+  './manifest.json',
+  './js/app.js',
+  './icon-192.png',
+  './icon-512.png',
+  // Add audio files here if you want to cache them
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      })
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResp => {
-      return cachedResp || fetch(event.request);
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
     })
   );
 });
